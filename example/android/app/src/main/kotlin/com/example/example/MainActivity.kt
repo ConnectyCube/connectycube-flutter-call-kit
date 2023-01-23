8dfad5c8090a1459897b2e191c6a0cd8cc3e893e
Julian Bissekkou <julian.bissekkou@tapped.dev>
2023-01-23 14:22:56 +0100

    fix platform version

--- example/android/app/src/main/kotlin/com/example/example/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/example/example/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.connectycube.flutter.connectycube_flutter_call_kit_example
+package com.example.example
 
 import io.flutter.embedding.android.FlutterActivity
 
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,25 +1,18 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
-
-import 'package:flutter/services.dart';
-import 'package:connectycube_flutter_call_kit/connectycube_flutter_call_kit.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
   @override
-  _MyAppState createState() => _MyAppState();
+  State<MyApp> createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
   String _platformVersion = 'Unknown';
-
-  @override
-  void initState() {
-    super.initState();
-  }
 
   @override
   Widget build(BuildContext context) {
