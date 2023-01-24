5688a25c7cd115742df6187cbce6dc6b400fd234
Stefan Schaller <stefan.schaller@tapped.dev>
2023-01-24 13:34:46 +0100

    change data types to string

--- android/src/main/kotlin/com/connectycube/flutter/connectycube_flutter_call_kit/EventReceiver.kt
+++ android/src/main/kotlin/com/connectycube/flutter/connectycube_flutter_call_kit/EventReceiver.kt
@@ -58,8 +58,8 @@ class EventReceiver : BroadcastReceiver() {
             ACTION_CALL_ACCEPT -> {
                 val extras = intent.extras
                 val callId = extras?.getString(EXTRA_CALL_ID)
-                val callType = extras?.getInt(EXTRA_CALL_TYPE)
-                val callInitiatorId = extras?.getInt(EXTRA_CALL_INITIATOR_ID)
+                val callType = extras?.getString(EXTRA_CALL_TYPE)
+                val callInitiatorId = extras?.getString(EXTRA_CALL_INITIATOR_ID)
                 val callInitiatorName = extras?.getString(EXTRA_CALL_INITIATOR_NAME)
                 val callOpponents = extras?.getIntegerArrayList(EXTRA_CALL_OPPONENTS)
                 val userInfo = extras?.getString(EXTRA_CALL_USER_INFO)
@@ -68,8 +68,8 @@ class EventReceiver : BroadcastReceiver() {
                 val broadcastIntent = Intent(ACTION_CALL_ACCEPT)
                 val bundle = Bundle()
                 bundle.putString(EXTRA_CALL_ID, callId)
-                bundle.putInt(EXTRA_CALL_TYPE, callType!!)
-                bundle.putInt(EXTRA_CALL_INITIATOR_ID, callInitiatorId!!)
+                bundle.putString(EXTRA_CALL_TYPE, callType!!)
+                bundle.putString(EXTRA_CALL_INITIATOR_ID, callInitiatorId!!)
                 bundle.putString(EXTRA_CALL_INITIATOR_NAME, callInitiatorName)
                 bundle.putIntegerArrayList(EXTRA_CALL_OPPONENTS, callOpponents)
                 bundle.putString(EXTRA_CALL_USER_INFO, userInfo)
